60ddb4b77191dcc399421d7941dcadf248379d5b
MATTHEW JERCINOVIC <103994960@student.swin.edu.au>
2022-04-26 08:42:44 +1000

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace oo_cookies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
